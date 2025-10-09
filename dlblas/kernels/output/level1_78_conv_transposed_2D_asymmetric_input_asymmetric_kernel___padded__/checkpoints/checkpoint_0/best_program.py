332dc88d19c81ac3f7147a52ed06402320d647c0
quanliyao <quanliyao@pjlab.org.cn>
2025-10-09 17:00:47 +0800

    addOpenEvolveOutput1

--- dlblas/kernels/output/level1_78_conv_transposed_2D_asymmetric_input_asymmetric_kernel___padded__/checkpoints/checkpoint_0/best_program.py
+++
@@ -1,2 +0,0 @@
-h_offsets = block_h_idx + tl.arange(0, BLOCK_H)
-    w_offsets = block_w_idx + tl.arange(0, BLOCK_W)
--- dlblas/kernels/output/level1_7_Matmul_with_small_K_dimension_/checkpoints/checkpoint_0/best_program.py
+++
@@ -1,6 +0,0 @@
-mask_m = offs_m < M
-    mask_n = offs_n < N
-    mask_k = offs_k < K
-
-    a = tl.load(a_ptrs, mask=mask_m[:, None] & mask_k[None, :], other=0.0)
-    b = tl.load(b_ptrs, mask=mask_k[:, None] & mask_n[None, :], other=0.0)
