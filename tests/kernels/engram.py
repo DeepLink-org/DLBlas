9c5b448f2368a178f08083bd5563b9aa9090c973
Pan Daoxin <pandaoxin@pjlab.org.cn>
2026-03-03 03:46:16 +0000

    Fix code style.

--- tests/kernels/engram.py
+++ tests/kernels/engram.py
@@ -94,7 +94,6 @@ class EngramPt(nn.Module):
         return output
 
 
-
 # ----------------------------------------------------------------------
 #  Test utilities (unchanged)
 # ----------------------------------------------------------------------
@@ -135,13 +134,16 @@ def get_inputs():
 def get_init_inputs():
     return [engram_hidden_size, hidden_size, kernel_size, dilation, hc_mult]
 
+
 def test_engram():
     embeddings_pt, hidden_states_pt = get_inputs()
     embeddings_tri = embeddings_pt.clone()
     hidden_states_tri = hidden_states_pt.clone()
 
     torch.manual_seed(41)
-    engram_tri = EngramTri(engram_hidden_size, hidden_size, kernel_size, dilation, hc_mult)
+    engram_tri = EngramTri(
+        engram_hidden_size, hidden_size, kernel_size, dilation, hc_mult
+    )
     hidden_states_tri = (
         engram_tri(embeddings=embeddings_tri, hidden_states=hidden_states_tri)
         + hidden_states_tri
@@ -149,7 +151,9 @@ def test_engram():
     print(hidden_states_tri)
 
     torch.manual_seed(41)
-    engram_pt = EngramPt(engram_hidden_size, hidden_size, kernel_size, dilation, hc_mult)
+    engram_pt = EngramPt(
+        engram_hidden_size, hidden_size, kernel_size, dilation, hc_mult
+    )
     hidden_states_pt = (
         engram_pt(embeddings=embeddings_pt, hidden_states=hidden_states_pt)
         + hidden_states_pt
